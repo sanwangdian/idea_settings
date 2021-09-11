89a0fd4569d38107cbf1645352f7c147e61d12db
sanwangdian <1048339086@qq.com>
2021-09-11 22:54:30 +0800

    second test

--- maven-test/src/main/java/com/sanwangdian/HelloDemo.java
+++ maven-test/src/main/java/com/sanwangdian/HelloDemo.java
@@ -3,5 +3,7 @@ package com.sanwangdian;
 public class HelloDemo {
     public static void main(String[] args) {
         System.out.println("hello");
+
+        System.out.println("ceshi");
     }
 }
